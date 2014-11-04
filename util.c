ptesarik/kdumpid

commit e1a8f69a238f1efa2dc1e693513c1366912ffe0e

Get rid of DIF_XEN
The libkdumpfile helper should be used instead.

Signed-off-by: Petr Tesarik <petr@tesarici.cz>

util.c
@@ -228,7 +228,7 @@ explore_kernel(struct dump_desc *dd, explore_fn fn)
 	if (arch_in_array(dd->arch, x86_biarch)) {
 		/* Xen pv kernels are loaded low */
 		addr = 0x2000;
-		if (dd->flags & DIF_XEN &&
+		if (dd->flags & kdump_is_xen(dd->ctx) &&
 		    looks_like_kcode_x86(dd, addr) > 0 &&
 		    !fn(dd, addr, addr + MAX_KERNEL_SIZE, x86_biarch)) {
 			dd->start_addr = addr;

x86.c
@@ -182,7 +182,7 @@ disas_at(struct dump_desc *dd, struct disassemble_info *info, unsigned pc)
 			if (cont)
 				continue;
 
-			if (!state.depth && dd->flags & DIF_XEN
+			if (!state.depth && kdump_is_xen(dd->ctx)
 			    && state.flags & SI_STORED) {
 				if (state.flags & SP_MODIFIED &&
 				    looks_like_kvaddr(info, state.sp_value))
@@ -218,7 +218,7 @@ disas_at(struct dump_desc *dd, struct disassemble_info *info, unsigned pc)
 				return 1;
 			if (is_reg(arg1, "si")) {
 				state.flags |= SI_STORED;
-				if (dd->flags & DIF_XEN &&
+				if (dd->flags & kdump_is_xen(dd->ctx) &&
 				    !(state.flags & SI_MODIFIED) &&
 				    sscanf(arg2, "0x%llx", &a) == 1)
 					dd->xen_start_info = a;

kdumpid.h
@@ -40,7 +40,6 @@ struct dump_desc {
 /* Kdumpid flags */
 #define DIF_VERBOSE	1
 #define DIF_FORCE	2
-#define DIF_XEN		4
 #define DIF_START_FOUND	8
 
 /* Arch-specific helpers */

commit a557c16904df5ca29eec27494c43470e662fb8e7

Fix wrong usage of kdump_is_xen()
Fix a regression from commit e1a8f69a238f1efa2dc1e693513c1366912ffe0e.
The whole expression "dd->flags & DIF_XEN" is equivallent to
"kdump_is_xen(dd->ctx)". Performing a logical AND operation with
the program flags only causes random failures.

Signed-off-by: Petr Tesarik <petr@tesarici.cz>

util.c
@@ -228,7 +228,7 @@ explore_kernel(struct dump_desc *dd, explore_fn fn)
 	if (arch_in_array(dd->arch, x86_biarch)) {
 		/* Xen pv kernels are loaded low */
 		addr = 0x2000;
-		if (dd->flags & kdump_is_xen(dd->ctx) &&
+		if (kdump_is_xen(dd->ctx) &&
 		    looks_like_kcode_x86(dd, addr) > 0 &&
 		    !fn(dd, addr, addr + MAX_KERNEL_SIZE, x86_biarch)) {
 			dd->start_addr = addr;

x86.c
@@ -218,7 +218,7 @@ disas_at(struct dump_desc *dd, struct disassemble_info *info, unsigned pc)
 				return 1;
 			if (is_reg(arg1, "si")) {
 				state.flags |= SI_STORED;
-				if (dd->flags & kdump_is_xen(dd->ctx) &&
+				if (kdump_is_xen(dd->ctx) &&
 				    !(state.flags & SI_MODIFIED) &&
 				    sscanf(arg2, "0x%llx", &a) == 1)
 					dd->xen_start_info = a;